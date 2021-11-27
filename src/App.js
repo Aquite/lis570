f9800295d64503fa1837f247577b0cf687d16c73
Aquite <pavel.batalov@hotmail.com>
2021-11-27 11:21:43 -0800

    image covers

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Header from "./header/header-mini.js";
 import MapChart from "./MapChart";
 import TTest from "./TTest.js";
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   const [dataLoc, loadingLoc] = useFetch(
@@ -23,11 +26,18 @@ function App() {
         <p>Test page</p>
       </header>
       <main>
-        {loadingLoc ? <p>Loading...</p> : <MapChart dataLoc={dataLoc} />}
+        {loadingLoc ? (
+          <p>Loading...</p>
+        ) : (
+          <div>
+            <MapChart dataLoc={dataLoc} />
+          </div>
+        )}
         {loadingLoc || loadingLib ? (
           <p>Loading...</p>
         ) : (
           <React.Fragment>
+            <p>P-values:</p>
             <TTest dataLib={dataLib} dataLoc={dataLoc} metric={"Taught"} />
             <TTest dataLib={dataLib} dataLoc={dataLoc} metric={"Rural"} />
             <TTest
@@ -35,6 +45,7 @@ function App() {
               dataLoc={dataLoc}
               metric={"Conservative"}
             />
+            <TTest dataLib={dataLib} dataLoc={dataLoc} metric={"TrumpWin"} />
           </React.Fragment>
         )}
       </main>
